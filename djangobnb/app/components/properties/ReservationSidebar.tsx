983943fe200025b5374c2b08f3c209e578014713
robim16 <carteagajimenez@gmail.com>
2025-06-24 17:06:25 -0500

    creacion del comp. calendar

--- djangobnb/app/components/properties/ReservationSidebar.tsx
+++ djangobnb/app/components/properties/ReservationSidebar.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react"
 import { Range } from 'react-date-range'
+import {differenceInDays, eachDayOfInterval} from "date-fns"
 
 import apiService from "@/app/services/apiService"
 import useLoginModal from "../hooks/useLoginModal"
@@ -39,6 +40,30 @@ const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
   const [guests, setGuests] = useState<string>('1')
   const guestsRange = Array.from({ length: property.guests }, (_, index) => index + 1)
 
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      const dayCount = differenceInDays(
+        dateRange.endDate,
+        dateRange.startDate
+      )
+
+      if (dayCount && property.price_per_night) {
+        const _fee = ((dayCount * property.price_per_night) / 100) * 5
+
+        setFee(_fee)
+        setTotalPrice((dayCount * property.price_per_night) + _fee)
+        setNights(dayCount)
+      }
+      else {
+        const _fee = (property.price_per_night / 100) * 5
+
+        setFee(_fee)
+        setTotalPrice(property.price_per_night + _fee)
+        setNights(1)
+      }
+    }
+  }, [dateRange])
+
   return (
     <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
       <h2 className="mb-5 text-2xl">
@@ -66,14 +91,14 @@ const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
 
       <div className="mb-4 flex justify-between align-center">
         <p>Djangobnb fee</p>
-        <p>$40</p>
+        <p>${fee}</p>
       </div>
 
       <hr />
 
       <div className="mt-4 flex justify-between align-center font-bold">
         <p>Total</p>
-        <p>$840</p>
+        <p>${totalPrice}</p>
       </div>
     </aside>
   )
