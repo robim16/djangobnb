18817f13624186bdd3ed649299e3edc01ffa42b5
robim16 <carteagajimenez@gmail.com>
2025-07-08 17:06:10 -0500

    cambios en inbox page

--- djangobnb/app/inbox/page.tsx
+++ djangobnb/app/inbox/page.tsx
@@ -1,11 +1,42 @@
+import { getUserId } from "../lib/actions"
+import apiService from "../services/apiService"
+import React, { useState, useEffect } from "react"
 import Conversation from "../components/inbox/Conversation"
 
-const InboxPage = () => {
+export type UserType = {
+    id: string, 
+    name: string, 
+    avatar_url: string
+}
+
+export type ConversationType = {
+    id: string,
+    users: UserType[]
+}
+
+const InboxPage = async () => {
+    const userId = await getUserId()
+
+    if (!userId) {
+        return (
+            <main className="max-w-[1500px] max-auto px-6 py-12">
+
+            </main>
+        )
+    } 
+
+    const conversations = await apiService.get('/api/chat')
+
     return (
         <main className='max-w-[1500px] mx-auto px-6 pb-6 space-y-4'>
-            <h1 className="my-6 text-2xl">My Reservations</h1>
+            <h1 className="my-6 text-2xl">Inbox</h1>
 
-            <Conversation />
+            {conversations.map((conversation: ConversationType) => {
+                return (
+                    <Conversation />
+                )
+            })}
+
         </main>
     )
 }
